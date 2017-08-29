f75d363e4cb2e4c88d351aaa8d12466daaf3408d
ankitagarwl <ankitagarwl@hotmail.com>
2017-08-29 14:20:52 +0530

    ok

--- tryzomato/Controllers/CategoryController.cs
+++ tryzomato/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using MongoDB.Driver;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace tryzomato.Controllers
 {
     public class CategoryController : Controller
     {
-        
+
         // GET: Category
 
         public async Task<ActionResult> Collections()
@@ -30,9 +31,6 @@ namespace tryzomato.Controllers
             }
 
         }
-
-
-        
         public async Task<ActionResult> Index()
         {
             using (var client = new HttpClient())
@@ -46,7 +44,7 @@ namespace tryzomato.Controllers
             }
 
         }
-        
+
         public async Task<ActionResult> HotelList(int collection_id)
         {
             using (var client = new HttpClient())
@@ -80,7 +78,7 @@ namespace tryzomato.Controllers
 
             using (var client = new HttpClient())
             {
-                var url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=" ; //city is static 1 for delhi,ncr
+                var url = "https://developers.zomato.com/api/v2.1/restaurant?res_id="; //city is static 1 for delhi,ncr
                 client.DefaultRequestHeaders.Add("user-key", "56fdd28b4835a8f484612f78f06ae582");
                 var response = await client.GetStringAsync(url);
                 RootObject_restaurant list = JsonConvert.DeserializeObject<RootObject_restaurant>(response);
@@ -91,15 +89,51 @@ namespace tryzomato.Controllers
 
         public async Task<ActionResult> Login()
         {
-           
 
-           
-                return View();
-           
+
+
+            return View();
+
 
         }
 
 
+
+
+        public ActionResult ar()
+        {
+            #region Without Credentials
+            //Specify Connection string 
+            var connectionString = "mongodb://localhost";
+            var mongoClient = new MongoClient(connectionString);
+            MongoServer mongoServer = mongoClient.GetServer();
+            var db = mongoServer.GetDatabase("hockey");
+            var categories = db.GetCollection("players").FindAll();
+            List<playerslist> list = new List<playerslist>();
+            foreach (var lstcategory in categories)
+            {
+                list.Add(new playerslist
+                {
+                    age = Convert.ToInt32(lstcategory["age"]),
+                    birthdate = Convert.ToString(lstcategory["birthdate"]),
+                    birthplace = Convert.ToString(lstcategory["birthplace"]),
+                    height = Convert.ToString(lstcategory["height"]),
+                    imageUrl = Convert.ToString(lstcategory["imageUrl"]),
+                    name = Convert.ToString(lstcategory["name"]),
+                    number = Convert.ToInt32(lstcategory["number"]),
+                    position = Convert.ToString(lstcategory["position"]),
+                    //twitterHandle = Convert.ToString(lstcategory["twitterHandle"]),
+                    //twitterURL = Convert.ToString(lstcategory["twitterURL"]),
+                    weight = Convert.ToInt32(lstcategory["weight"])
+
+                });
+                
+
+
+            }
+            #endregion
+            return View(list);
+        }
 
 
 
---
+++ tryzomato/playerslist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tryzomato
+{
+    public class playerslist
+    {
+        
+            public string position { get; set; }
+            public int id { get; set; }
+            public string twitterURL { get; set; }
+            public int weight { get; set; }
+            public string height { get; set; }
+            public string imageUrl { get; set; }
+            public string birthplace { get; set; }
+            public string twitterHandle { get; set; }
+            public int age { get; set; }
+            public string name { get; set; }
+            public string birthdate { get; set; }
+            public int number { get; set; }
+    
+
+    }
+}
